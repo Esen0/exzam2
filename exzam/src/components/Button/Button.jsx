93cbf03e09c868f724f9aaa7af1d6f19277df2f4
akylbekh <akylbakirov32@gmail.com>
2023-03-22 14:34:26 +0600

    modified:   exzam/index.html 	modified:   exzam/src/App.css 	modified:   exzam/src/App.jsx 	new file:   exzam/src/components/Button/Button.jsx 	new file:   exzam/src/components/Button/Button.module.css 	new file:   exzam/src/components/Card/Card.jsx 	new file:   exzam/src/components/Card/Card.module.css 	modified:   exzam/src/components/Fond/Fond.jsx 	modified:   exzam/src/components/Fond/Fond.module.css 	new file:   exzam/src/components/Footer/Footer.jsx 	new file:   exzam/src/components/Footer/Footer.module.css 	modified:   exzam/src/components/Nav/Nav.module.css 	new file:   exzam/src/components/img/animation.png 	new file:   exzam/src/components/img/avatar.png 	new file:   exzam/src/components/img/image 10.png 	new file:   exzam/src/components/img/image 12.png 	new file:   exzam/src/components/img/image 7 (1).png 	new file:   exzam/src/components/img/image 7.png 	new file:   exzam/src/components/img/image 9.png

---
+++ exzam/src/components/Button/Button.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from './Button.module.css'
+
+
+function Button(props) {
+    return (
+        <button className={btnType(props.type)}>{props.text}</button>
+    )
+}
+
+function btnType(type) {
+    if(type == 'norm'){
+        return styles.btn
+    }else if (type == 'blue'){
+        return styles.blue
+    }
+    else if (type == 'red'){
+        return styles.red
+    }
+}
+export default Button;
---
+++ exzam/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Footer.module.css' 
+
+const Footer = () => {
+  return (
+    <div className={styles.block6}>
+         <h1 className={styles.title_h2}>Токен<span>добра</span></h1>
+        <p className={styles.beta1}>beta</p>
+        </div>
+  )
+}
+
+export default Footer
